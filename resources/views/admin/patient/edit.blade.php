7da27b6c5c01f1f0cff9e13f7e7db633bfb3dd3f
Maria <maria.piontkowski@hotmail.com>
2017-11-17 14:47:12 -0200

    Botão de copiar

--- resources/views/admin/patient/edit.blade.php
+++ resources/views/admin/patient/edit.blade.php
@@ -9,10 +9,15 @@
     <div class="row clearfix">
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
             <div class="card">
-                <div class="header">
-                    <h2>
+                <div class="header" style="height: 60px;">
+                    <h2 class="pull-left">
                         Editar Paciente
                     </h2>
+                    <button class="btn btn-xs btn-copy waves-effect pull-right" title="Copiar Paciente"
+                    data-toggle="tooltip" data-placement="left"
+                    data-clipboard-action="copy" data-clipboard-text="{{$patient->name}}">
+                    <i class="material-icons">content_copy</i>
+                    </button>
                 </div>
                 <div class="body">
                     @include('admin.patient.form')
@@ -21,3 +26,10 @@
         </div>
     </div>
 @endsection
+
+@push("scripts")
+<script src="{{ asset("plugins/clipboardjs/clipboard.min.js") }}"></script>
+<script>
+    new Clipboard('.btn-copy');
+</script>
+@endpush
